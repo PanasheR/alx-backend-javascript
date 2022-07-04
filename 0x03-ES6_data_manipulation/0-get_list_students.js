d0bf748649c0664f82b97823afa7b57a80baaa28
Panashe Rusakaniko <panasherussy10@gmail.com>
2022-07-05 00:04:29 +0200

    Basic list of objects

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,11 +1,18 @@
-function getListStudents() {
-  const obj = [
-    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' },
-  ];
-
-  return obj;
-}
-
+const getListStudents = () => [
+  {
+    id: 1,
+    firstName: 'Guillaume',
+    location: 'San Francisco',
+  },
+  {
+    id: 2,
+    firstName: 'James',
+    location: 'Columbia',
+  },
+  {
+    id: 5,
+    firstName: 'Serena',
+    location: 'San Francisco',
+  },
+];
 export default getListStudents;

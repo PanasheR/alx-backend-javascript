0c011fc6623fa56f14b53ad6367e761e1cd1edee
Panashe Rusakaniko <panasherussy10@gmail.com>
2022-07-04 16:15:00 +0200

    Second class

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,9 @@
+import ClassRoom from './0-classroom';
+
+const initializeRooms = () => {
+  const arr = [19, 20, 34];
+
+  return arr.map((n) => new ClassRoom(n));
+};
+
+export default initializeRooms;

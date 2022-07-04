670debba4ee51191564deeefc607ae71a3b9dcc1
Panashe Rusakaniko <panasherussy10@gmail.com>
2022-07-04 16:49:57 +0200

    Removed whitespace

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -3,27 +3,27 @@ export default class Pricing {
     this._amount = amount;
     this._currency = currency;
   }
-  
+
   set amount(amount) {
     this._amount = amount;
   }
-  
+
   get amount() {
     return this._amount;
   }
-  
+
   set currency(currency) {
     this._currency = currency;
   }
-  
+
   get currency() {
     return this._currency;
   }
-  
+
   displayFullPrice() {
     return `${this.amount} ${this.currency.displayFullCurrency()}`;
   }
-  
+
   static convertPrice(amount, conversionRate) {
     return amount * conversionRate;
   }

f62f500840040b6df4cf583a9db41c9156e8c331
Panashe Rusakaniko <panasherussy10@gmail.com>
2022-07-04 15:16:29 +0200

    hundreth promise

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,17 @@
+import { createUser, uploadPhoto } from './utils';
+
+const asyncUploadUser = async () => {
+  let result = { photo: null, user: null, };
+
+  try {
+    result = {
+      photo: await uploadPhoto(),
+      user: await createUser(),
+    };
+    return result;
+  } catch (error) {
+    return result;
+  }
+};
+
+export default asyncUploadUser;

a91220242bae33a9549fa8aa79d99837024f3291
Panashe Rusakaniko <panasherussy10@gmail.com>
2022-07-04 16:24:56 +0200

    Fixed trailing spaces

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -4,33 +4,33 @@ export default class HolbertonCourse {
     this._length = length;
     this._students = students;
   }
-  
+
   get name() {
     return this._name;
   }
-  
+
   set name(name) {
     if (typeof name === 'string')
       this._name = name;
     else
       throw new TypeError('Name must be a string');
   }
-  
+
   get length() {
     return this._length;
   }
-  
+
   set length(length) {
     if (typeof length === 'number')
       this._length = length;
     else
       throw new TypeError('Length must be a number');
   }
-  
+
   get students() {
     return this._students;
   }
-  
+
   set students(students) {
     if (
       Array.isArray(students)

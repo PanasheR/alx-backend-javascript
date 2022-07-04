0c955f7b4c073efa43a5718de5126527e60db41d
Panashe Rusakaniko <panasherussy10@gmail.com>
2022-07-04 16:41:21 +0200

    Removed all curly brackets

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,28 +1,26 @@
 export default class Currency {
   constructor(code, name) {
-    this.code = code;
-    this.name = name;
+    this._code = code;
+    this._name = name;
+  }
+
+  set code(code) {
+    this._code = code;
   }
 
   get code() {
     return this._code;
   }
 
-  set code(code) {
-    if (typeof code === 'string')
-      this._code = code;
+  set name(name) {
+    this._name = name;
   }
 
   get name() {
     return this._name;
   }
 
-  set name(name) {
-    if (typeof name === 'string')
-      this._name = name;
-  }
-
   displayFullCurrency() {
-    return `${this.name} (${this.code})`;
+    return `${this._name} (${this._code})`;
   }
 }
